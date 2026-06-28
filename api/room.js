8a15b2195ae7c8b8f2d75b37dafffe200971d825
missqtpye <121464835+missqtpye@users.noreply.github.com>
2026-06-27 20:00:22 -0500

    Update room.js

--- api/room.js
+++ api/room.js
@@ -1,1 +1,8 @@
+const { getRoom } = require("../lib/store");
 
+module.exports = (req, res) => {
+  const code = (req.query.code || "").toUpperCase();
+  const room = getRoom(code);
+  if (!room) return res.status(404).json({ error: "Room not found" });
+  res.status(200).json({ state: room });
+};
